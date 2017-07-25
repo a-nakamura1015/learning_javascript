a32372c7b9ae9171eac57c074393e13d5cd46e7f
a-nakamura <a-nakamura@esm.co.jp>
2017-07-25 13:24:38 +0900

    arr２からarr2へ修正(全角から半角へ)

--- the_eight_chapter/8.2/ex08-02-4/main.js
+++ the_eight_chapter/8.2/ex08-02-4/main.js
@@ -7,6 +7,6 @@ console.log(arr); // [1, 2, 3, 4, 5, 6, 7]
 console.log(arr2); //[]
 arr2 = arr.splice(1, 2); // arr[1]から２個削除
 console.log(arr); // [1, 4, 5, 6, 7]
-console.log(arr２); // [2, 3]　←削除された要素
+console.log(arr2); // [2, 3]　←削除された要素
 arr2 = arr.splice(2, 1, 'a', 'b', 6 ,7);
 console.log(arr2); // [5] ←削除された要素
